adefda60cd79172423baeb37bf241547ab95ee92
Sunnidino <sbriannaparker@gmail.com>
2018-04-22 21:13:58 -0500

    updates added

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -14,32 +14,40 @@ findTime();
 //Write a JavaScript program to convert a number to a string.
 //Create a funtion to return
 function numToString(num1,num2){
-  return ('The winner is number') + ' ' +(num1 + num2);
+  return num1 + num2;
 }
-console.log(numToString(4,5));
+console.log(numToString('4','5'));
 
 //Write a JavaScript program to convert a string to the number.
 //Function to return string of numbers
 function stringToNum(){
-  return (10+5+6);
+  return ('10'+'5'+'6');
 }
 console.log(stringToNum());
 
 //Write a JavaScript program that takes in different datatypes and prints out whether they are a:
 //Boolean
-function getUpdate(){
-  return true; //function return true statement
+
+function time(){
+  if (8 >= 10) {
+    return 'Go to class';
+  }else {
+    return 'Class is over';
+  }
 }
-console.log(getUpdate());
+console.log (time()); //Create a function to determine is 8 is greater than 10 and return false statement.
+
 //Null
-function findUpdate(){
- return null;  // Create a function with
+
+const dataTwo = null;
+function findUpdate(dataTwo){
+ return (dataTwo * 2);  //
 }
-console.log(findUpdate());
+console.log(findUpdate(null));  //Create a funtion that does not have a value.
 
 //Undefined = Create a function to return a variable with an unassigned value
-function  add(){
-  return ''; //create function return without a value
+function  add(a){
+  return a; //create function return without a value
 }
 console.log(add());
 
@@ -50,7 +58,7 @@ function addNums(num){
 console.log(addNums(2));
 //NaN
 function data(){
-  return 1+1;
+  return (a+1);  //Function will return NaN due to the variable 'a'.
 }
 console.log (data());
 
@@ -59,44 +67,47 @@ function profileString(name,city){
   return (`My name is ${name}. I live in ${city}`);//return with String Interpolation (wrap variable with ${}
 }
 console.log(profileString('Sunny','Austin'));
-const name = 'Sunny';
-const city = 'Austin';
-console.log(name + ' ' + city);
+
 //Write a JavaScript program that adds 2 numbers together.
+
 function addTwoNum(add1,add2){
   return add1 + add2; // funtion returns add1+add2
 }
 console.log(addTwoNum(2,4));
-const add1 = 2;
-const add2 = 4;
-console.log(add1+add2);
+
 //Write a JavaScript program that runs only when 2 things are true.
+
 function twoTrue(a,b){
   return a && b; //return both parameters a and b as true.
 }
-console.log(twoTrue('run','truthy'));
-const a = 'run';
-const b = 'truthy';
+const a = 'red';
+const b = 'blue';
 if (a && b ) {
-  console.log('Run');
+  console.log('red');
 } else {
-  console.log ('True')
+  console.log ('blue');
 }
+console.log (twoTrue(a,b));
+
 //Write a JavaScript program that runs when 1 of 2 things are true.
 
 function eitherTrue(a,b){
 if(a || b){
-  console.log('run'); //returns one or the other.
+  console.log('red'); //returns one or the other.
 }else {
-  console.log('True');
-}
+  console.log('blue');
+ }
 }
 console.log(eitherTrue(b));
 //Write a JavaScript program that runs when both things are not true.
-function noneWork(){    //Need work on understanding Boolean function
-  if (noneWork){
-    console.log('Try Again');
-  }else{
-    console.log('No, Try Again');
+
+function noneWork(){    //Function included else if statment to exclude all possible statements
+  if (1 >= 5) {
+   return 'Try Again';
+  } else if (2 >= 5){
+    return 'Not this one';
+  } else {
+    return 'No, Try Again';
   }
 }
+console.log(noneWork());
